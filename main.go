28013ba20951873e27c4e47cf018f41d2875257d
hupe1980 <frankhuebner1980@gmail.com>
2021-12-04 01:24:14 +0100

    Fix linter

--- main.go
+++ main.go
@@ -89,11 +89,13 @@ func main() {
 
 func isFlagPassed(name string) bool {
 	found := false
+
 	flag.Visit(func(f *flag.Flag) {
 		if f.Name == name {
 			found = true
 			return
 		}
 	})
+
 	return found
 }
